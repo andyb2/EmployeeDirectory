d6650674f5be3ea7512a63df9d116e776e99479f
AMB <a.michael.boyle@gmail.com>
2021-03-30 15:33:04 -0400

    changed search

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Employee from './employee'
+
 
 const Search = (props) => {
     return (
@@ -7,8 +7,8 @@ const Search = (props) => {
             <form>
                 <div className="form-group search-widget">
                     <div className="input-group mb-3">
-                        <input className='form-control' value={props.search} name='search' type='text' placeholder='Search directory for Employee' id='search' />
-                        <button className="btn btn-primary" onclick={Employee.employeeSearch}>Search</button>
+                        <input className='form-control' onChange={e => props.employeeSearch(e)} name='search' type='text' placeholder='Search directory for Employee' id='search' />
+
                     </div>
                 </div>
             </form>
